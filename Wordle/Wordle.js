ed44d202d06a6799738d8bed647e8bee5ee9fb82
Jason Bui <143408649+JasonKrain@users.noreply.github.com>
2024-07-28 12:28:11 +0000

    revert back to keydown

--- Wordle/Wordle.js
+++ Wordle/Wordle.js
@@ -31,7 +31,7 @@ var winFlag = [0,0,0,0,0];
 var stopGame = 0;
 
 
-document.addEventListener("keypress", function(event){
+document.addEventListener("keydown", function(event){
         var key = event.key.toUpperCase();
         var keycode = event.keycode;
 
