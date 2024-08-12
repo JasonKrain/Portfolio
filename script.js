2992694aecf814b7586d3ce3f64ba0fe802feb3b
Jason Bui <143408649+JasonKrain@users.noreply.github.com>
2024-08-12 10:39:46 +0000

    changes to scrollheight for background image transition + downloadCV button functionality + extra projects

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ document.addEventListener("scroll",function() {
     document.getElementById("transition-image").style.opacity = opacity;
   }
 
-  var aboutMeHeight = (document.getElementById("about-me").scrollHeight)*0.65 + document.getElementById("transition-section").scrollHeight + introHeight;
+  var aboutMeHeight = (document.getElementById("about-me").scrollHeight)*0.65 + document.getElementById("transition").scrollHeight + document.getElementById("projects").scrollHeight ;
 
   if(remaining > aboutMeHeight) {
     document.getElementById("transition-image").style.bottom = (remaining - aboutMeHeight)*0.11 + "%";
